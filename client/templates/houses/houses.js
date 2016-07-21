41bca422b4cd04bf1b4a74fae9f36cc4cf15a384
devds <oleh.duts@gmail.com>
2016-07-21 17:48:35 +0300

    modify addHouse.v2

--- client/templates/houses/houses.js
+++ client/templates/houses/houses.js
@@ -5,16 +5,24 @@ Template.houses.helpers ({
 Template.add_house.events ({
     'submit #house-add': (e, template) => {
         e.preventDefault();
-        Houses.insert({
+        function addImg(img) {
+            houseInfo.img = img;
+            Houses.insert(houseInfo);
+        }
+        let houseInfo = {
             title: template.find('.house-title').value,
             country: template.findAll('#country-select option')[template.find('#country-select').selectedIndex].value,
             city: template.find('.house-city').value,
             street: template.find('.house-street').value,
             number: template.find('.house-number').value,
-            checkIn: template.findAll('#check-in-select option')[template.find('#check-in-select').selectedIndex].value
-        });
+            checkIn: template.findAll('#check-in-select option')[template.find('#check-in-select').selectedIndex].value,
+            checkOut: template.findAll('#check-out-select option')[template.find('#check-out-select').selectedIndex].value,
+            wifi: template.find('input[name=internet-check]:checked').value
+        };
+        base64(template.find('#imgUpload'), addImg);
     }
 });
 Template.add_house.onRendered( () => {
     $('select').material_select();
+    $('.tooltipped').tooltip({delay: 50});
 });
