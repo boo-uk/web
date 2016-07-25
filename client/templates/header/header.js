be56eac781b88eebba7de772739e4cd8665322b8
Юра <yuraantonov11@gmail.com>
2016-07-25 23:14:50 +0300

    publish

--- client/templates/header/header.js
+++
@@ -1,7 +0,0 @@
-Template.header.helpers({
-    userMail: () => Meteor.user().emails[0].address
-});
-
-Template.header.onRendered(function() {
-    setTimeout(() => $(this.find('.dropdown-button')).dropdown(), 2000);
-});
