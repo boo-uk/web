024710aa5fc6b0160490257fb9a2c01c5e601dbb
yuraantonov <yuraantonov11@gmail.com>
2016-08-01 18:00:34 +0300

    bad localization

--- client/templates/header/header.js
+++ client/templates/header/header.js
@@ -1,7 +1,26 @@
 Template.Header.helpers({
-    userMail: () => Meteor.user().emails[0].address
+    userMail: () => Meteor.user().emails[0].address,
+    langListEn: () => _.map(TAPi18n.languages_names, (b) => {
+        return b[0];
+    }),
+    langList: () => _.map(TAPi18n.languages_names, (b) => {
+        return b[1];
+    }),
+    langCodes: _.keys(TAPi18n.languages_names)
+});
+Template.LangSwitcher.helpers({
+    langListEn: () => _.map(TAPi18n.languages_names, (b) => {
+        return b[0];
+    }),
+    langList: () => _.map(TAPi18n.languages_names, (b) => {
+        return b[1];
+    }),
+    langCodes: _.keys(TAPi18n.languages_names)
 });
 
 Template.Header.onRendered(function() {
-    setTimeout(() => $(this.find('.dropdown-button')).dropdown(), 2000);
+    setTimeout(function() {
+        $(this.find('.dropdown-button')).dropdown();
+        $('select').material_select();
+    }, 200);
 });
